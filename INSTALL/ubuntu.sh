92f6552228c01403efe9d774d4e75b0a7fb37375
Moe <github@m03.ca>
2017-10-30 22:51:24 -0700

    update installers to be more verbose

--- INSTALL/ubuntu.sh
+++ INSTALL/ubuntu.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+echo "========================================================="
+echo "==!! Shinobi : The Open Source CCTV and NVR Solution !!=="
+echo "========================================================="
+echo "To answer yes type the letter (y) in lowercase and press ENTER."
+echo "Default is no (N). Skip any components you already have or don't need."
+echo "============="
 echo "Shinobi - Do you want to Install Node.js?"
 echo "(y)es or (N)o"
 read nodejsinstall
@@ -7,14 +13,19 @@ if [ "$nodejsinstall" = "y" ]; then
     chmod +x setup_8.x
     ./setup_8.x
     sudo apt install nodejs -y
-    rm setup_8.x
 fi
 echo "============="
-echo "Shinobi - Get FFMPEG 3.x from ppa:jonathonf/ffmpeg-3"
-sudo add-apt-repository ppa:jonathonf/ffmpeg-3 -y
-sudo apt update -y && sudo apt install ffmpeg libav-tools x264 x265 -y
+echo "Shinobi - Do you want to Install FFMPEG?"
+echo "(y)es or (N)o"
+read ffmpeginstall
+if [ "$ffmpeginstall" = "y" ]; then
+    echo "Shinobi - Get FFMPEG 3.x from ppa:jonathonf/ffmpeg-3"
+    sudo apt-get install software-properties-common python-software-properties -y
+    sudo add-apt-repository ppa:jonathonf/ffmpeg-3 -y
+    sudo apt update -y && sudo apt install ffmpeg libav-tools x264 x265 -y
+fi
 echo "============="
-echo "Shinobi - Do you want to Install MariaDB? Choose No if you have MySQL."
+echo "Shinobi - Do you want to Install MariaDB? Choose No if you have MySQL or MariaDB already."
 echo "(y)es or (N)o"
 read mysqlagree
 if [ "$mysqlagree" = "y" ]; then
@@ -23,10 +34,9 @@ if [ "$mysqlagree" = "y" ]; then
     read sqlpass
     echo "mariadb-server mariadb-server/root_password password $sqlpass" | debconf-set-selections
     echo "mariadb-server mariadb-server/root_password_again password $sqlpass" | debconf-set-selections
-    apt install mariadb-server -y
-    service mysql start
+    sudo apt install mariadb-server -y
+    sudo service mysql start
 fi
-chmod -R 755 .
 echo "============="
 echo "Shinobi - Database Installation"
 echo "(y)es or (N)o"
@@ -41,9 +51,9 @@ if [ "$mysqlagreeData" = "y" ]; then
         echo "What is your SQL Password?"
         read sqlpass
     fi
-    mysql -u $sqluser -p$sqlpass -e "source sql/user.sql" || true
-    mysql -u $sqluser -p$sqlpass -e "source sql/framework.sql" || true
-    echo "Shinobi - Do you want to Install Default Data (default_data.sql)?"
+    sudo mysql -u $sqluser -p$sqlpass -e "source sql/user.sql" || true
+    sudo mysql -u $sqluser -p$sqlpass -e "source sql/framework.sql" || true
+    echo "Shinobi - Do you want to create a new user for viewing and managing cameras in Shinobi? You can do this later in the Superuser panel."
     echo "(y)es or (N)o"
     read mysqlDefaultData
     if [ "$mysqlDefaultData" = "y" ]; then
@@ -62,7 +72,8 @@ if [ "$mysqlagreeData" = "y" ]; then
         apiDetails=$(echo "$apiDetails" | sed -e 's/"/'$escapeReplaceQuote'/g')
         rm sql/default_user.sql || true
         echo "USE ccio;INSERT INTO Users (\`ke\`,\`uid\`,\`auth\`,\`mail\`,\`pass\`,\`details\`) VALUES (\"$groupKey\",\"$userID\",\"$apiKey\",\"$userEmail\",\"$userPasswordMD5\",\"$userDetails\");INSERT INTO API (\`code\`,\`ke\`,\`uid\`,\`ip\`,\`details\`) VALUES (\"$apiKey\",\"$groupKey\",\"$userID\",\"$apiIP\",\"$apiDetails\");" > "sql/default_user.sql"
-        mysql -u $sqluser -p$sqlpass --database ccio -e "source sql/default_user.sql" > "INSTALL/log.txt"
+        sudo mysql -u $sqluser -p$sqlpass --database ccio -e "source sql/default_user.sql" > "INSTALL/log.txt"
+        echo "The following details will be shown again at the end of the installation."
         echo "====================================="
         echo "=======!! Login Credentials !!======="
         echo "|| Username : $userEmail"
@@ -75,25 +86,43 @@ if [ "$mysqlagreeData" = "y" ]; then
 fi
 echo "============="
 echo "Shinobi - Install NPM Libraries"
-npm install
+sudo npm install
 echo "============="
 echo "Shinobi - Install PM2"
 sudo npm install pm2 -g
 if [ ! -e "./conf.json" ]; then
-    cp conf.sample.json conf.json
+    sudo cp conf.sample.json conf.json
 fi
 if [ ! -e "./super.json" ]; then
     echo "Default Superuser : admin@shinobi.video"
     echo "Default Password : admin"
-    cp super.sample.json super.json
+    sudo cp super.sample.json super.json
 fi
 echo "Shinobi - Finished"
+sudo chmod -R 755 .
 touch INSTALL/installed.txt
+echo "=====================================" > INSTALL/installed.txt
+echo "=======!! Login Credentials !!=======" >> INSTALL/installed.txt
+echo "|| Username : $userEmail" >> INSTALL/installed.txt
+echo "|| Password : $userPasswordPlain" >> INSTALL/installed.txt
+echo "|| API Key : $apiKey" >> INSTALL/installed.txt
+echo "=====================================" >> INSTALL/installed.txt
+echo "=====================================" >> INSTALL/installed.txt
 echo "Shinobi - Start Shinobi?"
 echo "(y)es or (N)o"
 read startShinobi
 if [ "$startShinobi" = "y" ]; then
-    pm2 start camera.js
-    pm2 start cron.js
-    pm2 list
+    sudo pm2 start camera.js
+    sudo pm2 start cron.js
+    sudo pm2 startup
+    sudo pm2 save
+    sudo pm2 list
 fi
+echo "details written to INSTALL/installed.txt"
+echo "====================================="
+echo "=======!! Login Credentials !!======="
+echo "|| Username : $userEmail"
+echo "|| Password : $userPasswordPlain"
+echo "|| API Key : $apiKey"
+echo "====================================="
+echo "====================================="
