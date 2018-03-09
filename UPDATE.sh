da9f70a20b1c1211cf29163214049b74690db73a
Moe <github@m03.ca>
2018-03-09 15:45:35 -0800

    remove restart from updater

--- UPDATE.sh
+++ UPDATE.sh
@@ -21,14 +21,3 @@ rm version.json
 touch version.json
 chmod 755 version.json
 echo '{"Product" : "'"$productName"'" , "Branch" : "'"$distro"'" , "Version" : "'"$gitVersionNumber"'" , "Date" : "'"$theDateRightNow"'" , "Repository" : "'"$gitURL"'"}' > version.json
-pm2 restart camera.js
-pm2 restart cron.js
-echo "-------------------------------------"
-echo "---------- Shinobi Systems ----------"
-echo "Repository : $gitURL"
-echo "Product : $productName"
-echo "Branch : $distro"
-echo "Version : $gitVersionNumber"
-echo "Date : $theDateRightNow"
-echo "-------------------------------------"
-echo "-------------------------------------"
