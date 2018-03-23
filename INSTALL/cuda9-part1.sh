f4f7c739e7feb896fae0d0a9d8b8415b212e2e2f
Moe <github@m03.ca>
2018-03-22 22:30:58 -0700

    add missing update command for cuda

--- INSTALL/cuda9-part1.sh
+++ INSTALL/cuda9-part1.sh
@@ -3,6 +3,7 @@ sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/
 sudo echo "# NVIDIA Graphics Driver Repo (Added by Shinobi installer)" >> /etc/apt/sources.list
 sudo echo "# Public Key : sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub" >> /etc/apt/sources.list
 sudo echo "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64 /" >> /etc/apt/sources.list
+sudo apt update
 sudo apt-get -y install cuda-drivers
 
 echo "After rebooting you need to run part 2. The file is named `cuda9-part2-after-reboot.sh`."
