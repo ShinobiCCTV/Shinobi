b44f4d869278bce90818d03ba93e82401f83401d
Soljia <soljia+github@gmail.com>
2018-04-11 15:12:29 -0400

    Update camera.js

--- camera.js
+++ camera.js
@@ -8,6 +8,7 @@
 // If you like what I am doing here and want me to continue please consider donating :)
 // PayPal : paypal@m03.ca
 //
+var fs = require('fs');
 process.on('uncaughtException', function (err) {
     console.error('Uncaught Exception occured!');
     console.error(err.stack);
@@ -15,11 +16,13 @@ process.on('uncaughtException', function (err) {
 var ffmpegPath = false;
 try{
     ffmpegPath = require('ffmpeg-static').path;
+    if (!fs.existsSync(ffmpegPath)) {
+	    throw "file doesn't exist!"
+    }
 }catch(err){
     console.log('No Static FFmpeg. Continuing.')
     //no static ffmpeg
 }
-var fs = require('fs');
 var os = require('os');
 var URL = require('url');
 var path = require('path');
