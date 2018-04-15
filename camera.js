6a49da24638c251cbde0c8bc11153762a286694d
Moe <github@m03.ca>
2018-04-14 19:14:58 -0700

    Fix Non-Monotonous DTS Error
    found by Kevin Godell

--- camera.js
+++ camera.js
@@ -1268,6 +1268,7 @@ s.ffmpeg=function(e){
         case'h264':
             switch(e.protocol){
                 case'rtsp':
+                    if(e.details.cust_input.indexOf('-fflags') === -1){x.cust_input+=' -fflags +nobuffer+igndts'}
                     if(e.details.rtsp_transport&&e.details.rtsp_transport!==''&&e.details.rtsp_transport!=='no'){x.cust_input+=' -rtsp_transport '+e.details.rtsp_transport;}
                 break;
             }
