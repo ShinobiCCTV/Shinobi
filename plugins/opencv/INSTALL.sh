12d3f67fe5d5be6485d60f9708584a8c942100d6
Moe <github@m03.ca>
2018-04-30 09:18:59 -0700

    update opencv plugin installer

--- plugins/opencv/INSTALL.sh
+++ plugins/opencv/INSTALL.sh
@@ -1,1 +1,8 @@
-export OPENCV4NODEJS_DISABLE_AUTOBUILD=1
+#!/bin/bash
+echo "Shinobi - Do ypu want to let the `opencv4nodejs` npm package install OpenCV? Only do this if you will not use a GPU (Hardware Acceleration)."
+echo "(y)es or (N)o"
+read nodejsinstall
+if [ "$nodejsinstall" = "y" ] || [ "$nodejsinstall" = "Y" ]; then
+    export OPENCV4NODEJS_DISABLE_AUTOBUILD=1
+fi
+npm install opencv4nodejs moment express canvas@1.6 --unsafe-perm
